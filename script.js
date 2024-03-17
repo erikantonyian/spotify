11b8c9c95c10e43500be69ffb52f37f4579f542f
erikantonyian <108613162+erikantonyian@users.noreply.github.com>
2024-03-17 14:35:30 +0400

    Update script.js

--- script.js
+++ script.js
@@ -75,13 +75,6 @@ function isMobileDevice() {
     return /Mobi|Android/i.test(navigator.userAgent);
 }
 
-// if (!isMobileDevice()) {
-//     console.log('This is not a mobile device.');
-//     // home.remove();
-//     // player.remove();
-//     // splash.remove();
-// }
-
 
 if (params.get("playlist_id")) {
     home.hidden = true;
